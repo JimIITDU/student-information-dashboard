de709bec44938da87f2da432a172c31f7ebce80d
JimIITDU <your-official-email@iit.du.ac.bd>
2026-04-09 01:25:28 +0600

    successfully done with setup of backend server and database configuration in render

---
+++ backend/config/database.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require('sequelize');
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '../.env') });
+
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  logging: false
+});
+
+module.exports = sequelize;
